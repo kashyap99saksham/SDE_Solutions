22137ae021da6bfca92fc30a2488c5f632d83dd6
Saksham Kashyap <kprincekashyap7@gmail.com>
2020-08-25 02:43:55 +0530

    Update Container with most water.cpp

--- ARRAY/Container with most water.cpp
+++ ARRAY/Container with most water.cpp
@@ -1,3 +1,9 @@
+// Approach: Given an array of heights of lines of boundaries of the container, 
+// find the maximum water that can be stored in a container. So start with the first and last element and check the amount of water that can be contained and store that container. 
+// Now the question arises is there any better boundaries or lines that can contain maximum water. So there is a clever way to find that.
+// Initially, there are two indices the first and last index pointing to the first and the last element (which acts as boundaries of the container),
+// if the value first index is less than the last index increase the first index else decrease the last index. As the increase in width is constant, 
+// by following the above process the optimal answer can be reached.
 int maxArea(vector<int>& height) 
     {
         int i = 0,j=height.size()-1,ans=-1;
